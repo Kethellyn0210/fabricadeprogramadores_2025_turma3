1053f43d55049df3531d1a49defa8dc5c31e2af1
Kethellyn0210 <ketf270@gmail.com>
2025-09-26 14:40:59 -0300

    Add files via upload

--- projeto_faculdade/main.py
+++ projeto_faculdade/main.py
@@ -2,55 +2,50 @@ from tabelas import SessionLocal, Usuario, Nota
 
 db = SessionLocal()
 
-def criar_novo_usuario_e_nota():
-    """Exemplo de como CRIAR dados."""
-    novo_usuario = Usuario(
-    nome = "Kethellyn Fernanda",
-    email = "Ket@email.com",
-    senha_hash = "hash_super_seguro" 
-    )
+def criar_novo_usuario_e_nota(novo_usuario: Usuario, nova_nota: Nota):
 
     db.add(novo_usuario)
     db.commit()
-    print(f"Usuário '{novo_usuario}.nome' criado com ID: {novo_usuario}")
+    print(f"Usuario '{novo_usuario.nome}' criado com ID: {novo_usuario.id}")
 
-    nova_nota = Nota(
-    titulo = "Minha Primeira Nota com SQLAlchemy",
-    contudo = "É muito mais fácil do que escrever SQL na mão!",
-    autor = novo_usuario 
-    )
     db.add(nova_nota)
-    db.commit()
-    print(f"Nota '{nova_nota.titulo}' criada para {nova_nota.nome}.")
+    db.commit ()
 
-def ler_dados(nome):
-    """Exemplo de como LER dados."""
 
-    user = db.query(Usuario).filter(Usuario.nome == nome).first()
-
-    if user:
-        print(f"Enconteri o(a): {user.nome} (Email: {user.email})")
- 
-        print("Notas de user:")
-        for nota in user.notas:
-            print(f" -Título: {nota.titulo} (ID: {nota.id})")
-    else:
-        print("Usuário(a) não encontrado.")
-    
-def atualizar_nota(id_nota):
-    """Exemplo de como ATUALIZAR dados."""
-
+def autualizar_nota(id_nota: int,titulo:str ,conteudo:str):
     nota_para_editar = db.query(Nota).filter(Nota.id == id_nota).first()
 
     if nota_para_editar:
-        print(f"Título original: '{nota_para_editar.titulo}'")
 
-        nota_para_editar.titulo = "Lista de anotações ATULIZADA!"
+        nota_para_editar.titulo = titulo
+        nota_para_editar.conteudo = conteudo
 
         db.commit()
-        print(f"Título novo: '{nota_para_editar.titulo}'")
-
+    
     else:
-        print("Nota com ID %d nçao encontrada." % id_nota)
+        print("Nota com ID % não encontrada." % id_nota)
 
-criar_novo_usuario_e_nota()
+def ler_dados():
+
+    users = db.query(Usuario).all()
+
+    if users:
+        
+        for nota in users.notas:
+            print(f"  - conteudo: {nota.conteudo} (ID {nota.id})")
+        return users , nota
+    else:
+        print("Usuario(a) nao encontrado.")
+
+def deletar_usuario(id_usuario: int):
+
+    usuario_deletado = db.query(Usuario).filter(Usuario.id == id_usuario).first()
+
+    if usuario_deletado:
+
+        db.delete(usuario_deletado)
+        db.commit()
+
+        print(f"Usuario: '{usuario_deletado.nome}' removido com sucesso!")       
+    else:
+        print("Nota com ID % não encontrada." % id_usuario)
--- projeto_faculdade/tabelas.py
+++ projeto_faculdade/tabelas.py
@@ -1,8 +1,12 @@
 import datetime
 from sqlalchemy import *
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from dotenv import load_dotenv, dotenv_values
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/postgres"
+load_dotenv()
+
+variaveis_de_ambiente = dotenv_values()
+DATABASE_URL = variaveis_de_ambiente ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 
@@ -11,19 +15,20 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 base = declarative_base()
 
 class Usuario (base):
-    __tablename__ = "usuarios" 
+    _tablename_ = "usuarios" 
     
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String(100), nullable=False)
-    email = Column (String(255), unique= True, nullable=False)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nome = Column(String(255), nullable=False)
+    email = Column (String(255), nullable=False)
     senha_hash = Column (String(255), nullable=False)
     criado_em = Column (DateTime(timezone=True), default=datetime.datetime.now)
+    
     notas = relationship("Nota", back_populates="autor")
 
-class Nota (base):
-    __tablename__ = "notas"
+class Nota(base):
+    _tablename_ = "notas"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     id_usuario = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
     conteudo = Column(Text)
     criado_em = Column(DateTime(timezone=True), default=datetime.datetime.now)
@@ -31,6 +36,6 @@ class Nota (base):
 
     autor = relationship("Usuario", back_populates="notas")
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     base.metadata.create_all(bind=engine)
     print("Tabelas criadas com sucesso (se não existiam).")
